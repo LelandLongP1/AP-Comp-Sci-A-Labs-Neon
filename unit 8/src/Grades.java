78adb170145602e9a095d206ef78031da9f8da99
Leland <Lelandlong@gmail.com>
2018-02-22 23:29:37 -0800

    work on unit 8

--- unit 8/src/Grades.java
+++ unit 8/src/Grades.java
@@ -5,27 +5,44 @@
 //Lab  -
 
 import java.lang.System;
+import java.text.DecimalFormat;
 import java.lang.Math;
 import java.util.Scanner;
 
 public class Grades
 {
 	//instance variables
+	private int size = 0;
+	Scanner keyboard = new Scanner(System.in);
+	double [] finalList;
+	private static DecimalFormat df2 = new DecimalFormat(".##");
 	
-	
-
 	//constructor
-
-
+	public Grades(int num){
+		setArray(num);
+		
+	}
+	
 
 	//set method
+	public void setArray(int num){
+		size = num;
+		double gradeList[] = new double[size];
+		for (int gradeNum = 0; gradeNum < size; gradeNum++){
+			System.out.println("Enter grade " + (gradeNum + 1));
+			gradeList[gradeNum] = keyboard.nextDouble();
+		}
+		finalList = gradeList;
+	}
 
 
 
 	private double getSum()
 	{
 		double sum=0.0;
-
+		for (int i = 0; i < finalList.length; i++){
+			sum = sum + finalList[i];
+		}
 
 
 		return sum;
@@ -34,7 +51,7 @@ public class Grades
 	public double getAverage()
 	{
 		double average=0.0;
-
+		average = getSum() / finalList.length;
 
 
 		return average;
@@ -42,7 +59,15 @@ public class Grades
 
 
 	//toString method
-
+	public String toString(){
+		String output = "";
+		for (int i = 0; i < size; i++){
+			output = output + "\n" + "Grade " + i + " :: " + finalList[i];
+		}
+		
+		output = output + "\n\n" + "average: " + df2.format(getAverage());
+		return output;
+	}
 
 
 }
--- unit 8/src/Lab14d.java
+++ unit 8/src/Lab14d.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -8,6 +10,12 @@ public class Lab14d
 {
 	public static void main( String args[] )
 	{
+		int num = 0;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("How many grades do you want to input?");
+		num = keyboard.nextInt();
+		Grades test = new Grades(num);
+		System.out.println(test);
 		//add test cases
 		
 		
