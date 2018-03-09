7ca4ed9fa82ea769a675e6092baadb0ffaccc0d1
bonelessweeb <lelandlong@gmail.com>
2018-03-09 09:30:45 -0800

    unit 10 work

--- unit 10/src/Histogram.java
+++ unit 10/src/Histogram.java
@@ -14,7 +14,7 @@ import static java.lang.System.*;
 
 public class Histogram
 {
-	private ArrayList<Integer> count;
+	private ArrayList<Integer> count = new ArrayList<Integer>();
 	private ArrayList<Character> letters = new ArrayList<Character>();
 	private String fileName = "";
 
@@ -28,17 +28,35 @@ public class Histogram
 
 	public Histogram(char[] values, String fName)
 	{
+		fileName = fName;
 		
-		for (int i = 0; i < values.length; i++){
-			letters.add(values[i]);
+		for (int i = 0 ; i < values.length; i++){
+			letters.add(values[i]); 
+			count.add(0);
 		}
+		
 		out.println("search letters = "+letters);
+		
 	}
 
 	public void loadAndAnalyzeFile() throws IOException
 	{
-
-
+		Scanner file = new Scanner(new File(fileName));
+		for (int i = 0; i < letters.size(); i++){
+			int number = 0;
+			while(file.hasNext()){
+				String word = file.next(); 
+				for (int k = 0; k < word.length(); k++){
+					if (word.charAt(k) == letters.get(i)){
+						number = number + 1;
+						count.set(i, number);
+					}//if statement
+					
+				}//check
+				
+			}//while loop
+			
+		}//for loop
 
 
 
@@ -49,15 +67,15 @@ public class Histogram
 
 	public char mostFrequent()
 	{
-
-		return '#';
+		
+		return letters.get(count.indexOf(Collections.max(count)));
 	}
 
 	public char leastFrequent()
 	{
 
 
-		return '#';
+		return letters.get(count.indexOf(Collections.min(count)));
 	}
 
 	public String toString()
